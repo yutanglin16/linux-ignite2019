e62383d883b2d3c422a21f4bad9822e0ad6faf9c
Yutang Lin <10567485+yutanglin16@users.noreply.github.com>
2019-10-30 22:40:09 -0700

    edit health check

--- routes/healthcheck.js
+++ routes/healthcheck.js
@@ -9,11 +9,13 @@ router.get('/', function(req, res, next) {
     if (isHealthy() == true) {
         const msg = "App is healthy 👍."
         console.log(msg);
-        res.status(200).send(msg);
+        // res.status(200).send(msg);
+        res.status(200).json(msg);
     } else {
         const msg = "App is unhealthy 👎.";
         console.warn(msg)
-        res.status(500).send(msg);
+        // res.status(500).send(msg);
+        res.status(500).json(msg);
     }
 });
 
